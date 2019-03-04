266f1e052a021ab2d94c174b787ff1c3265ce1c9
Marcel Hofer <the.hoferm@gmail.com>
2019-03-04 14:53:08 +0100

    Add default redirect if no route matches

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { ContactsListComponent } from "./contacts-list/contacts-list.component";
+import { Route } from "@angular/router";
 
-export const APP_ROUTES = [
-    { path: '', component: ContactsListComponent}
+export const APP_ROUTES: Route[] = [
+    { path: '', component: ContactsListComponent },
+    { path: '**', redirectTo: '' }
 ];
