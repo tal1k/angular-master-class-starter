d5be75838ebdef8f8a47d0296cbd18ad368ba961
Marcel Hofer <the.hoferm@gmail.com>
2019-03-04 12:08:06 +0100

    Add trackBy

--- NOTES.md
+++ NOTES.md
@@ -5,11 +5,12 @@ Main links:
 - [Slides](https://classroom.thoughtram.io/#/class/angular-master-class) / [Repo Slides](https://github.com/thoughtram/angular-master-class-slides)
 
 Additional links:
-- Stackblitz
+- Stackblitz.com
 - [Angular Checklist](https://angular-checklist.io/projects)
 
 ## TODO
 - Watch [TreeShaking](https://www.youtube.com/watch?v=jnp_ny4SOQE)
+- `$0 / console.dir` in Developer console
 
 ## Intro
 - Zone.js für Change Detection (Push based, timeout / xhr requests werden überschrieben)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,8 @@ import { CONTACT_DATA } from 'src/app/data/contact-data';
   styleUrls: ['./app.component.scss']
 })
 export class ContactsAppComponent {
-  title = 'Angular Master Class';
-  contact: Contact = {
-    id: 6,
-    name: 'Diana Ellis',
-    email: '',
-    phone: '',
-    birthday: '',
-    website: '',
-    image: '/assets/images/6.jpg',
-    address: {
-      street: '6554 park lane',
-      zip: '43378',
-      city: 'Rush',
-      country: 'United States'
-    }
-  };
   contacts = CONTACT_DATA;
+  trackContact(index: number, contact: Contact) {
+    return contact.id;
+  }
 }
